6a066794ddd5a48ea5aee92f07c7daf98faa6d25
trollakatroll <gmhelper@gmail.com>
2021-10-22 10:46:30 -0400

    Board storage

--- Test/Game.java
+++ Test/Game.java
@@ -1,5 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 /**
  * Write a description of class Game here.
  * 
@@ -8,14 +12,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Game extends World
 {
-
-    /**
-     * Constructor for objects of class Game.
-     * 
-     */
+    int wordLength = 4;
+    int numOfWords = 5;
+    ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
     public Game()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        
+        for(int i= 0; i < numOfWords; i++){
+            ArrayList<String> temp = new ArrayList<String>();
+            board.add(temp);
+        }
+        ArrayList<String> words = Reader.read(wordLength);
+        Collections.shuffle(words);
+        for(int i = 0; i < numOfWords; i++){
+            //get a word and split
+            String[] strArr = words.get(i).split("");
+            //then add to each list???
+            for(int j = 0; j < wordLength; j++){
+                if(!board.get(j).contains(strArr[j])){
+                    board.get(j).add(strArr[j]);    
+                }
+            }
+        }
+        for(ArrayList<String> s : board){
+            Collections.shuffle(board);
+        }
     }
 }
---
+++ Test/Reader.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class Reader {
+    public static ArrayList<String> read(int stringLength) throws Exception {
+        ArrayList<String> arr = new ArrayList<String>();
+        try{
+            String url = "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt";
+            URL wordsURL = new URL(url);
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(wordsURL.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                if(inputLine.length() == stringLength)
+                    arr.add(inputLine);
+            in.close();
+        }catch(Exception e){
+            System.out.print("");
+        }
+        return arr;
+    }
+}
