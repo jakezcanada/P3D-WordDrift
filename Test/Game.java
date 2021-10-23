bd202ab2f8609894c607b76048f1dcf43187fc5e
trollakatroll <gmhelper@gmail.com>
2021-10-23 03:35:25 -0400

    Finished Game Logic
    Implemented the logic, still needs to be tweaked to become a visual element.

--- Test/Game.java
+++ Test/Game.java
@@ -10,34 +10,113 @@ import java.util.Scanner;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Game extends World
-{
-    int wordLength = 4;
-    int numOfWords = 5;
-    ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
-    public Game()
-    {    
+public class Game extends World{
+    class Block{
+        String item;
+        Boolean solved;
+    }
+    public Game(){    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        // State board "size"
+        int wordLength = 4;
+        int numOfWords = 5;
+        // Fetch words from the Reader class and randomize the words
+        ArrayList<String> words = Reader.read(wordLength);
+        Collections.shuffle(words);
+        // Create the game board
+        ArrayList<ArrayList<Block>> board = createBoard(wordLength, numOfWords, words);
+        // Randomize each columns' order
+        for(ArrayList<Block> s : board){
+            Collections.shuffle(board);
+        }
+        // selected will keep track of which Block is selected in each column
+        int[] selected = new int[wordLength];
+        Arrays.fill(selected, 0);
+        // selectedColumn will keep track of, who could've guesses, the selected column...
+        int selectedColumn = 0;
+        // navigation operations
+        if(Greenfoot.isKeyDown("right") && selectedColumn < wordLength-1){
+            selectedColumn++;
+        }else if(Greenfoot.isKeyDown("left") && selectedColumn > 0){
+            selectedColumn--;
+        }else if(Greenfoot.isKeyDown("up") && selected[selectedColumn] < board.get(selectedColumn).size()-1){
+            selected[selectedColumn]++;
+            String selectedStr = selectedString(board, selected);
+            if(words.contains(selectedStr)){
+                for(int i = 0; i < selected.length; i++){
+                    board.get(i).get(selected[i]).solved = true;
+                }
+            }
+        }else if(Greenfoot.isKeyDown("down") && selected[selectedColumn] > 0){
+            selected[selectedColumn]--;
+            String selectedStr = selectedString(board, selected);
+            if(words.contains(selectedStr)){
+                for(int i = 0; i < selected.length; i++){
+                    board.get(i).get(selected[i]).solved = true;
+                }
+            }
+        }
+
         
+        
+        
+    }
+    
+    public ArrayList<ArrayList<Block>> createBoard(int wordLength, int numOfWords, ArrayList<String> words){
+        // Create all new ArrayLists
+        ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
         for(int i= 0; i < numOfWords; i++){
             ArrayList<String> temp = new ArrayList<String>();
             board.add(temp);
         }
-        ArrayList<String> words = Reader.read(wordLength);
-        Collections.shuffle(words);
-        for(int i = 0; i < numOfWords; i++){
-            //get a word and split
-            String[] strArr = words.get(i).split("");
-            //then add to each list???
+        // Put the words into the board object
+        for(String str : words){
+            // Get a word and split
+            String[] strArr = str.split("");
+            // If the section doesn't already have said letter, add it
             for(int j = 0; j < wordLength; j++){
                 if(!board.get(j).contains(strArr[j])){
                     board.get(j).add(strArr[j]);    
                 }
             }
         }
-        for(ArrayList<String> s : board){
-            Collections.shuffle(board);
+        // Copy over into a 2D Block Array for efficient completion check
+        ArrayList<ArrayList<Block>> board2 = new ArrayList<ArrayList<Block>>();
+        for(int i= 0; i < numOfWords; i++){
+            ArrayList<Block> temp = new ArrayList<Block>();
+            board2.add(temp);
         }
+        for(int i= 0; i < wordLength; i++){
+            for(int j = 0; j < board.get(i).size(); j++){
+                Block temp = new Block();
+                temp.solved = false;
+                temp.item = board.get(i).get(j);
+                board2.get(i).add(temp);
+            }
+        }
+        
+        return board2;
     }
+    
+    public String selectedString(ArrayList<ArrayList<Block>> board, int[] selected){
+        String selectedString = "";
+        for(int i = 0; i < selected.length; i++){
+            selectedString += board.get(i).get(selected[i]).item;
+        }
+        return selectedString;
+    }
+    
+    // Check if the current board is fully "solved"
+    public boolean checkBoard(ArrayList<ArrayList<Block>> board){
+        for(ArrayList<Block> arr : board){
+            for(Block b : arr){
+                if(!b.solved){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
 }
--- Test/Reader.java
+++ Test/Reader.java
@@ -6,14 +6,15 @@ public class Reader {
     public static ArrayList<String> read(int stringLength){
         ArrayList<String> arr = new ArrayList<String>();
         try{
-            String url = "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt";
+            String url = "https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt";
             URL wordsURL = new URL(url);
             BufferedReader in = new BufferedReader(
             new InputStreamReader(wordsURL.openStream()));
             String inputLine;
+            // If the word has the correct length and isn't already contained in the array, add it
             while ((inputLine = in.readLine()) != null)
-                if(inputLine.length() == stringLength)
-                    arr.add(inputLine);
+                if(inputLine.length() == stringLength && !arr.contains(inputLine.toUpperCase()))
+                    arr.add(inputLine.toUpperCase());
             in.close();
         }catch(Exception e){
             System.out.print("");
