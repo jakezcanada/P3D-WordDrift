2407e2dc53e061a900512f9230bf9c1fe75ca3e9
chloehylee <335732582@gapps.yrdsb.ca>
2021-11-02 17:54:51 -0400

    Added Instructions Page

--- Test/Instructions.java
+++ Test/Instructions.java
@@ -17,6 +17,6 @@ public class Instructions extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1);
-        addObject(new Button(new GreenfootImage("ExitButton-2.png"), getHeight()/3, 4), getWidth()/2, getHeight()*4/5);
+        addObject(new Button(new GreenfootImage("WordDrift Instructions.png"), 650, 1.77), 640, 360);
     }
 }
